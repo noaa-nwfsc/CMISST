0327f5112303cc7cca7c7774cbdcac37161067ca
Aimee Fullerton <aimee.fullerton@noaa.gov>
2025-07-10 15:54:15 -0700

    Update crossValidation.R
    editorial

--- R/crossValidation.R
+++ R/crossValidation.R
@@ -12,15 +12,15 @@ LOO_CV <- function(response = response,
                    includePDO = FALSE, includePC1 = FALSE) {
 
   # Container for results
-  mae<-data.frame(model=as.character(), season=as.character(), year=as.numeric(),
+  mae <- data.frame(model=as.character(), season=as.character(), year=as.numeric(),
                     response=as.numeric(), pred=as.numeric(),
                     mae=as.numeric(), stringsAsFactors = FALSE)
   
   # Loop over just the last "loocvYears" years
-  n<-length(years)
+  n <- length(years)
   for (this_year in years[(n-(loocvYears-1)):n]) { # just the last "loocvYears" years
-    years.fit<-years[!years %in% this_year]
-    years.pred<-years[years %in% this_year]
+    years.fit <- years[!years %in% this_year]
+    years.pred <- years[years %in% this_year]
     
     # Get the index
     cmisst_loo <- get_CMISST_index(response = response,
@@ -33,8 +33,8 @@ LOO_CV <- function(response = response,
 
     # output
     index_cov <- cmisst_loo[[1]]
-    index.fit<-index_cov[index_cov$year %in% years.fit,]
-    index.pred<-index_cov[index_cov$year %in% years.pred,]
+    index.fit <- index_cov[index_cov$year %in% years.fit,]
+    index.pred <- index_cov[index_cov$year %in% years.pred,]
 
     # Calculate MAE
     # Need to loop over seasons
@@ -42,21 +42,21 @@ LOO_CV <- function(response = response,
       
       index.fit$var <- index.fit[,paste0(season,".cov")]
       index.pred$var <- index.pred[,paste0(season,".cov")]
-      mdl<-lm(val~var, data=index.fit)
-      pred<-predict(mdl, newdata = index.pred)
-      #cov_mae<-sqrt(mean((pred - index.pred$val)^2)) # This is RMSEP
-      #cov_mae<-mean(abs(pred - index.pred$val)) # This is MAE
-      cov_mae<-mean(abs(pred - response$val.scl[response$year == this_year])) # This is MAE
+      mdl <- lm(val~var, data=index.fit)
+      pred <- predict(mdl, newdata = index.pred)
+      #cov_mae <- sqrt(mean((pred - index.pred$val)^2)) # This is RMSEP
+      #cov_mae <- mean(abs(pred - index.pred$val)) # This is MAE
+      cov_mae <- mean(abs(pred - response$val.scl[response$year == this_year])) # This is MAE
       # Assign it to the data.frame
-      mae<-rbind(mae, data.frame(model="cmisst", season=season, year=this_year,
+      mae <- rbind(mae, data.frame(model="cmisst", season=season, year=this_year,
                                      response=response$val.scl[response$year == this_year], pred=pred,
                                      mae=cov_mae, stringsAsFactors = FALSE))
     } # End looping over season
   } # End looping over years
   
   se <- function(x) { return(sd(x)/sqrt(length(x))) }
-  mae$season<-factor(mae$season, levels = c("win","spr","sum","aut"))
-  # Trying this out - if I leave this line out, I might get all the individual predictions
-  if(exists("pred_out")) if (!pred_out) mae<-summaryBy(mae ~ model+season, data = mae, FUN = c(mean, se))
+  mae$season <- factor(mae$season, levels = c("win","spr","sum","aut"))
+  # The following line prevents outputting all the individual predictions:
+  if(exists("pred_out")) if (!pred_out) mae <- summaryBy(mae ~ model+season, data = mae, FUN = c(mean, se))
   return(list(mae))
 }
